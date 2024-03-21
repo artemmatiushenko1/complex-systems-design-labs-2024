87bf721eaa22025c39eff35fea33faa2c13b8174
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-03-21 21:52:06 +0200

    add testExecutionTime util

--- lab-3/src/main/kotlin/Utils.kt
+++ lab-3/src/main/kotlin/Utils.kt
@@ -1,6 +1,7 @@
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 inline fun <reified T> writeJsonFile(data: T, fileName: String): File {
     val outputFile = File(fileName)
@@ -22,4 +23,27 @@ inline fun <reified T> readJsonFileOrNull(fileName: String): T? {
     }
 
     return fileContent
+}
+
+fun testExecutionTime(
+    iterationsCount: Int,
+    step: Int,
+    initialN: Int,
+    outputFilePath: String,
+    testingCode: (n: Int) -> Unit
+) {
+    val statistics = mutableMapOf<Int, Long>()
+
+    for (n in initialN..(iterationsCount * initialN) step step) {
+        val executionTime = measureTimeMillis {
+            testingCode(n)
+        }
+
+        statistics[n] = executionTime
+        println("Completed in ${executionTime}ms.")
+        println()
+    }
+
+    val reportFile = writeJsonFile(statistics, outputFilePath)
+    println("Execution time report saved to: ${reportFile.absolutePath}")
 }
